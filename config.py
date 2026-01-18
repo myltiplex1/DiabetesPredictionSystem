16d8ed3005a91da985c9bb78f2f0c379b5e82ee5
Soyinka Enoch <timflash111@gmail.com>
2026-01-18 19:39:40 +0100

    config updated

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 MODEL_PATH = "diabetes_prediction_dataset.csv"
